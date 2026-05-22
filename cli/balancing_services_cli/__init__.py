0733cf1b8b27520be23fdd053b075c598f38e5aa
Vootele Rotov <vootele.rotov@gmail.com>
2026-05-22 14:41:37 +0300

    Remove support for CLI
    Was used by agents, but now superceded by MCP based solution.

--- cli/balancing_services_cli/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Balancing Services CLI - command-line access to European electricity balancing market data."""
-
-from importlib.metadata import version
-
-__version__ = version("balancing-services-cli")
--- cli/balancing_services_cli/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Allow running as python -m balancing_services_cli."""
-
-from balancing_services_cli.main import cli
-
-cli()
